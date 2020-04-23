fefc1743a7f768a62781013837c92b2b407fea97
Jonas J <57950820+Booom7@users.noreply.github.com>
2020-04-23 09:57:35 +0200

    Team konataktieren via email.

--- app/src/main/java/com/example/lostc/Team_kontaktieren.java
+++ app/src/main/java/com/example/lostc/Team_kontaktieren.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,8 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Team_kontaktieren extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,6 +26,7 @@ public class Team_kontaktieren extends AppCompatActivity implements View.OnClick
     private TextView tv_flaschenpost;
     private Button bt_absenden;
     private ImageButton ib_backTeamKontaktieren;
+    private TextInputEditText tiet_Inhalt;
 
 
     @Override
@@ -36,6 +40,8 @@ public class Team_kontaktieren extends AppCompatActivity implements View.OnClick
         et_name_aendern = findViewById(R.id.et_name_aendern);
         et_semester = findViewById(R.id.et_Semester);
         et_thema = findViewById(R.id.et_thema);
+        tiet_Inhalt = findViewById(R.id.tied_Inhalt);
+
         ib_backTeamKontaktieren = findViewById(R.id.ib_backTeamKontaktieren);
 
         ib_backTeamKontaktieren.setOnClickListener(this);
@@ -48,7 +54,33 @@ public class Team_kontaktieren extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.bt_absenden) {
-            //Jackermayer muss noch E-Mail verknüpfen
+            String to_email="test@.de"; //Hier bräuchten wir dann noch eine Email Adresse wo wir die Nachrichten hinsenden
+
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:"));
+            intent.putExtra(Intent.EXTRA_EMAIL,new String[] {to_email});
+
+            //Inhalt der email
+
+
+            String titel;
+            titel = et_thema.getText()+ "";
+            intent.putExtra(Intent.EXTRA_SUBJECT, titel);
+            String body;
+            String name;
+            name = "Name: "+ et_name_aendern.getText();
+            String semester;
+            semester = "Semester: "+ et_semester.getText();
+
+             String inhalt = tiet_Inhalt.getText() +"";
+            body = name + " \n" + semester + " \n" + inhalt;
+            intent.putExtra(Intent.EXTRA_TEXT, body);
+
+            startActivity(Intent.createChooser(intent, "Wähle eine Applikation"));
+
+
+
+
         }
         if(v.getId() == R.id.ib_backTeamKontaktieren) {
             openMainMenue();
